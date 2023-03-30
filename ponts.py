1c8b72974270c814d1017c7d824c73e54764e0f1
Thibault Roussel <thibaultroussel44@gmail.com>
2023-03-30 18:08:32 +0200

    essai 3

--- ponts.py
+++ ponts.py
@@ -3,6 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import psycopg2
+import sqlalchemy
+
 
 # URL de la page Wikipédia
 url = "https://fr.wikipedia.org/wiki/Liste_de_ponts_d%27Italie"
@@ -44,11 +46,13 @@ df = pd.DataFrame(data, columns=column_names)
 # Filtre des villes pour n'avoir que les ponts de la ville de "Gênes"
 df_filtered = df[df["localisation"].str.startswith("Gênes")]
 
-DATABASE_URL = os.environ['postgres://pjyflfrpaycgql:5babc4828af9a0749747e8f288ec088d99b8a8bd6647c904a12397b2131992ee@ec2-34-242-199-141.eu-west-1.compute.amazonaws.com:5432/daq7mhr06h7b1g']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# link to your database
+engine = sqlalchemy.create_engine(
+    'postgres://pjyflfrpaycgql:5babc4828af9a0749747e8f288ec088d99b8a8bd6647c904a12397b2131992ee@ec2-34-242-199-141.eu-west-1.compute.amazonaws.com:5432/daq7mhr06h7b1g',
+    echo=False)
+# attach the data frame (df) to the database with a name of the
+# table; the name can be whatever you like
+df_filtered.to_sql('base-donnee', conn=engine, if_exists='append')
+# run a quick test
+print(engine.execute("SELECT * FROM base-donnee").fetchone())
 
-df_filtered.to_sql('ponts', conn, if_exists='append', index=False)
-
-cur = conn.cursor()
-cur.execute("SELECT * FROM ponts;")
-rows = cur.fetchall()
