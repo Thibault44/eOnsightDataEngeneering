9c9a3e56ce8e2d03f3d32659e14423acbf58e8d6
Thibault Roussel <thibaultroussel44@gmail.com>
2023-04-01 12:41:54 +0200

    modification sql

--- ponts.py
+++ ponts.py
@@ -55,9 +55,9 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 for index, row in df_filtered.iterrows():
     cur.execute("INSERT INTO ponts (nom, longueur, bridge_type, voie_portée_franchie, date, localisation, region) "
-                "VALUES (%s, %s, %s, %s, %s, %s)",
+                "VALUES (%s, %s, %s, %s, %s, %s, %s)",
                 (row['nom'], row['longueur'], row['bridge_type'], row['voie_portée_franchie'], row['date'],
-                 row['localisation'], row['region']))
+                 row['localisation'], row['region'],))
 conn.commit()
 
 # Fermeture de la connexion
